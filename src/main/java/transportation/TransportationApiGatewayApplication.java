3e5c80e6bde175e1ac7be213293c181de2061738
vladdosiik <vladdosiik7540@gmail.com>
2023-01-04 15:22:16 +0300

    init

---
+++ src/main/java/transportation/TransportationApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package transportation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportationApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TransportationApiGatewayApplication.class, args);
+  }
+}
