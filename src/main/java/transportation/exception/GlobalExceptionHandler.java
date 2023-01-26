9be48b994d388b8f822de5a8236992a6eaa8d833
alice <lissa.faul@mail.ru>
2023-01-26 15:46:38 +0300

    TP-4 fixed models version

--- src/main/java/transportation/exception/GlobalExceptionHandler.java
+++ src/main/java/transportation/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package transportation.exception;
 
-import exeption.ErrorResponse;
+import exception.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
